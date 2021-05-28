76e6d484243d6730ccb2d49927c3933ca979650c
Muhammed Okumuş <rocketpolka@gmail.com>
2021-05-28 14:30:32 +0300

    return main

--- server.c
+++ server.c
@@ -76,6 +76,7 @@ int main(int argc, char *argv[])
   /*--------------Main loop-------------------------------*/
 
   /*--------------Free resources--------------------------*/
+  return 0;
 }
 
 void cleanup()
